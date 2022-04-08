265de48ad8eb6240dfbd3d6aae8134c9d3c198f4
DanihStephane <harilantodanih@gmail.com>
2022-04-08 21:49:43 +0300

    Gestion ekaly

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 export class AuthService {
 
   users: User[] = [{"username":"admin","password":"123","roles":['ADMIN']},
-                   {"username":"nadhem","password":"123","roles":['USER']} ];
+                   {"username":"danih","password":"123","roles":['USER']} ];
 
   public loggedUser:string;
   public isloggedIn: Boolean = false;
