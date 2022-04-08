97320073934033e9729f85f3ed223e463e0a7128
DanihStephane <harilantodanih@gmail.com>
2022-04-08 21:51:47 +0300

    Gestion livreur

--- frontend/restaurant/utils/tools.js
+++
@@ -1,7 +0,0 @@
-function extractToken(authorization){
-    if(!authorization) throw new Error("Pas d'autorisation");
-    var token = authorization.substring('Bearer ');
-    return token;
-}
-
-module.exports = {extractToken};
